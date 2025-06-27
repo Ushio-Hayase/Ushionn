b957a22a617edc9edc88ae3bf5558c75f0edadde
Ushio-Hayase <ushiohayase@gmail.com>
2025-06-27 16:23:44 +0900

    feat: perceptron test

--- tests/perceptron.cpp
+++ tests/perceptron.cpp
@@ -22,4 +22,39 @@ float forward(float* x, float* w, int len)
     return step(u);
 }
 
-void train(float* w)
+void train(float* w, float* x, float t, float e, int len)
+{
+    float z = forward(x, w, len);
+    for (int j = 0; j < len; ++j)
+    {
+        w[j] += (t - z) * x[j] * e;
+    }
+}
+
+constexpr int DATA_NUMS = 4;
+constexpr int WEIGHT_NUMS = 3;
+
+int main()
+{
+    float e = 0.1;
+
+    float x[DATA_NUMS][WEIGHT_NUMS] = {{1, 0, 0}, {1, 0, 1}, {1, 0, 1}, {1, 1, 1}};
+
+    float t[DATA_NUMS] = {0, 0, 0, 1};
+
+    float w[WEIGHT_NUMS] = {0, 0, 0};
+
+    int epoch = 10;
+    for (int i = 0; i < epoch; ++i)
+    {
+        std::cout << "Epoch : " << i << " ";
+        for (int j = 0; j < DATA_NUMS; ++j) train(w, x[j], t[j], e, WEIGHT_NUMS);
+
+        for (int j = 0; j < WEIGHT_NUMS; ++j) std::cout << "w" << j << ":" << w[j] << " ";
+
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < DATA_NUMS; ++i) std::cout << forward(x[i], w, WEIGHT_NUMS) << " ";
+    std::cout << std::endl;
+}
